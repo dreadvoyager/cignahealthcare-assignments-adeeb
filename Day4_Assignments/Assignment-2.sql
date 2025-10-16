5e819c12b295b578cc74395397227449642eb56e
Adeeb Khan <adeebahmedkhan02@gmail.com>
2025-10-16 15:15:52 +0530

    Update Assignment-2.sql

--- Day4_Assignments/Assignment-2.sql
+++ Day4_Assignments/Assignment-2.sql
@@ -1,1 +1,16 @@
-
+DECLARE
+  emp_name VARCHAR(50);
+  salary NUMBER;
+  bonus NUMBER;
+BEGIN 
+  emp_name := 'Vinni';
+  salary := 50000;
+  IF salary > 50000 THEN
+    bonus := 0.10*salary;
+  ELSE
+    bonus := 0.15*salary;
+  END IF;
+   DBMS_OUTPUT.PUT_LINE('Name : ' || emp_name);
+   DBMS_OUTPUT.PUT_LINE('Salary : ' || salary);
+   DBMS_OUTPUT.PUT_LINE('Bonus : ' || bonus);
+END;
